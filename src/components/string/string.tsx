68e5089f7d3e875a52ee130394f754e8a5c35b3a
Mike Starina <x@pnhd.ru>
2022-11-13 16:24:45 +0300

    fix

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -6,41 +6,30 @@ import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { circleStyle } from "../utils/string-utils";
 import { swap } from "../utils/string-utils";
-import styles from './string.module.css';
-
+import styles from "./string.module.css";
 
 export const StringComponent: React.FC = () => {
-
-
-  const [ lettersArray, setLettersArray ] = useState<string[]>([]);
-  const [ isLoading, setButtonLoader ] = useState<boolean>(false);
-  const [ modifiedIndexes, setModifiedIndexes ] = useState<number[]>([]);
-  const [ changingIndexes, setChangingIndexes ] = useState<number[]>([]);
-
+  const [lettersArray, setLettersArray] = useState<string[]>([]);
+  const [isLoading, setButtonLoader] = useState<boolean>(false);
+  const [modifiedIndexes, setModifiedIndexes] = useState<number[]>([]);
+  const [changingIndexes, setChangingIndexes] = useState<number[]>([]);
 
   const onChange = (event: FormEvent<HTMLInputElement>) => {
     event.preventDefault();
     setModifiedIndexes([]);
-    const arrayFromValue = event.currentTarget.value.split('');
+    const arrayFromValue = event.currentTarget.value.split("");
     setLettersArray(arrayFromValue);
-
   };
-  
-  
-
-
-
 
   const reverse = async (arr: string[]) => {
     setButtonLoader(true);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     for (let i = 0; i <= Math.floor(lettersArray.length / 2); i++) {
       const headIndex = i;
       const tailIndex = lettersArray.length - 1 - i;
       setChangingIndexes([headIndex, tailIndex]);
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       swap(lettersArray, headIndex, tailIndex);
       setChangingIndexes([]);
       const updatedIndexes = modifiedIndexes;
@@ -50,30 +39,42 @@ export const StringComponent: React.FC = () => {
 
     setLettersArray([...lettersArray]);
     setButtonLoader(false);
-  }
- 
+  };
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
 
     reverse(lettersArray);
-    
-    
-  }
+  };
 
   return (
     <SolutionLayout title="Строка">
       <form className={styles.input_container} onSubmit={onSubmit}>
-        <Input maxLength={11} height={60} type='text' onChange={onChange} isLimitText={true} autoFocus/>
-        <Button height={60} type='submit' text='Развернуть' isLoader={isLoading} />
+        <Input
+          maxLength={11}
+          height={60}
+          type="text"
+          onChange={onChange}
+          isLimitText={true}
+          autoFocus
+        />
+        <Button
+          height={60}
+          type="submit"
+          text="Развернуть"
+          isLoader={isLoading}
+        />
       </form>
 
       <div className={styles.result_container}>
-        {lettersArray && lettersArray.map((item, index) => (
-          <Circle key={index} letter={item} state={circleStyle(index, modifiedIndexes, changingIndexes)} />
-          
-        ))}
+        {lettersArray &&
+          lettersArray.map((item, index) => (
+            <Circle
+              key={index}
+              letter={item}
+              state={circleStyle(index, modifiedIndexes, changingIndexes)}
+            />
+          ))}
       </div>
     </SolutionLayout>
   );
--- src/components/utils/string-utils.tsx
+++ src/components/utils/string-utils.tsx
@@ -1,17 +1,25 @@
 import { ElementStates } from "../../types/element-states";
 
-export const swap = (arr: string[], headIndex: number, tailIndex: number): void => {
-    const temp = arr[headIndex];
-    arr[headIndex] = arr[tailIndex];
-    arr[tailIndex] = temp;
-}
+export const swap = (
+  arr: string[],
+  headIndex: number,
+  tailIndex: number
+): void => {
+  const temp = arr[headIndex];
+  arr[headIndex] = arr[tailIndex];
+  arr[tailIndex] = temp;
+};
 
-export const circleStyle = (index: number, modifiedIndexes: number[], changingIndexes: number[]) => {
-    if (modifiedIndexes.findIndex(item => item === index) !== -1) {
-      return ElementStates.Modified;
-    } else if (changingIndexes.findIndex(item => item === index) !== -1) {
-      return ElementStates.Changing;
-    } else {
-      return ElementStates.Default;
-    }
+export const circleStyle = (
+  index: number,
+  modifiedIndexes: number[],
+  changingIndexes: number[]
+) => {
+  if (modifiedIndexes.findIndex((item) => item === index) !== -1) {
+    return ElementStates.Modified;
+  } else if (changingIndexes.findIndex((item) => item === index) !== -1) {
+    return ElementStates.Changing;
+  } else {
+    return ElementStates.Default;
   }
+};
