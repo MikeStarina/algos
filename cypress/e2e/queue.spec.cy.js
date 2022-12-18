ba9b335a7b44c468459c05170672e9ea7bccdd32
Mike Starina <x@pnhd.ru>
2022-12-18 15:14:13 +0300

    ready for review 1

--- cypress/e2e/queue.spec.cy.js
+++ cypress/e2e/queue.spec.cy.js
@@ -1,151 +1,135 @@
+describe("queue page correct", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/queue");
+  });
 
+  it("Submit button disabled when input is empty", () => {
+    cy.get("form").within(() => {
+      cy.get("input").should("have.value", "");
+      cy.get('button[type="submit"]').should("be.disabled");
+    });
+  });
 
+  it("adding elements to queue correct", () => {
+    cy.clock();
+    cy.get("form").within(() => {
+      cy.get("input").type("1");
+      cy.get('button[type="submit"]').click();
+    });
 
+    cy.tick(1000);
 
-describe('queue page correct', () => {
+    cy.get('div[class*="circle_content"]').then((items) => {
+      cy.get(items[0])
+        .contains("head")
+        .should("have.text", "head");
+      cy.get(items[0])
+        .contains("tail")
+        .should("have.text", "tail");
+      cy.get(items[0])
+        .contains("1")
+        .should("have.text", "1");
+    });
 
-    beforeEach(() => {cy.visit('http://localhost:3000/queue')})
+    cy.get("form").within(() => {
+      cy.get("input").type("2");
+      cy.get('button[type="submit"]').click();
+      cy.tick(1000);
+      cy.get("input").type("3");
+      cy.get('button[type="submit"]').click();
+    });
 
+    cy.tick(1000);
 
-    it('Submit button disabled when input is empty', () => {
-      
-        cy.get('form')
-                .within(() => {
-                    cy.get('input').should('have.value', '')
-                    cy.get('button[type="submit"]').should('be.disabled')
-                })
-    })
+    cy.get('div[class*="circle_content"]').then((items) => {
+      cy.get(items[0])
+        .contains("head")
+        .should("have.text", "head");
+      cy.get(items[0])
+        .contains("1")
+        .should("have.text", "1");
+      cy.get(items[1])
+        .contains("2")
+        .should("have.text", "2");
+      cy.get(items[2])
+        .contains("tail")
+        .should("have.text", "tail");
+      cy.get(items[2])
+        .contains("3")
+        .should("have.text", "3");
+    });
+  });
 
+  it("deleting elements from queue correct", () => {
+    cy.clock();
+    cy.get("form").within(() => {
+      cy.get("input").type("1");
+      cy.get('button[type="submit"]').click();
+      cy.tick(1000);
+      cy.get("input").type("2");
+      cy.get('button[type="submit"]').click();
+      cy.tick(1000);
+      cy.get("input").type("3");
+      cy.get('button[type="submit"]').click();
+    });
 
-    it('adding elements to queue correct', () => {
-        cy.clock()
-        cy.get('form')
-        .within(() => {
-            cy.get('input').type('1')
-            cy.get('button[type="submit"]').click()
-        })
+    cy.tick(1000);
 
+    cy.get("form").within(() => {
+      cy.get("button")
+        .contains("Удалить")
+        .click();
+    });
+
+    cy.get('div[class*="circle_content"]').then((items) => {
+      cy.get(items[0]).within(() => {
+        cy.get('div[class*="circle_circle"]')
+          .invoke("attr", "class")
+          .then((classList) => expect(classList).contains("circle_changing"));
+      });
+    });
+
+    cy.tick(1000);
+
+    cy.get('div[class*="circle_content"]').then((items) => {
+      cy.get(items[0]).within(() => {
+        cy.get('div[class*="circle_circle"]').should("have.text", "");
+      });
+
+      cy.get(items[1])
+        .contains("head")
+        .should("have.text", "head");
+      cy.get(items[2])
+        .contains("tail")
+        .should("have.text", "tail");
+    });
+
+    it("clear queue", () => {
+      cy.clock();
+      cy.get("form").within(() => {
+        cy.get("input").type("1");
+        cy.get('button[type="submit"]').click();
         cy.tick(1000);
-
-        cy.get('div[class*="circle_content"]').then((items) => {
-            cy.get(items[0]).contains('head').should('have.text', 'head');
-            cy.get(items[0]).contains('tail').should('have.text', 'tail');
-            cy.get(items[0]).contains('1').should('have.text', '1');
-            
-        })
-
-        cy.get('form')
-        .within(() => {
-            cy.get('input').type('2')
-            cy.get('button[type="submit"]').click()
-            cy.tick(1000)
-            cy.get('input').type('3')
-            cy.get('button[type="submit"]').click()
-        })
-
+        cy.get("input").type("2");
+        cy.get('button[type="submit"]').click();
         cy.tick(1000);
+        cy.get("input").type("3");
+        cy.get('button[type="submit"]').click();
+      });
 
-        cy.get('div[class*="circle_content"]').then((items) => {
-            cy.get(items[0]).contains('head').should('have.text', 'head');
-            cy.get(items[0]).contains('1').should('have.text', '1');
-            cy.get(items[1]).contains('2').should('have.text', '2');
-            cy.get(items[2]).contains('tail').should('have.text', 'tail');
-            cy.get(items[2]).contains('3').should('have.text', '3');
-            
-        })
+      cy.tick(1000);
 
+      cy.get("form").within(() => {
+        cy.get("button")
+          .contains("Очистить")
+          .click();
+      });
 
-    })
-
-
-    it('deleting elements from queue correct', () => {
-        cy.clock()
-        cy.get('form')
-        .within(() => {
-            cy.get('input').type('1')
-            cy.get('button[type="submit"]').click()
-            cy.tick(1000)
-            cy.get('input').type('2')
-            cy.get('button[type="submit"]').click()
-            cy.tick(1000)
-            cy.get('input').type('3')
-            cy.get('button[type="submit"]').click()
-        })
-
-
-        cy.tick(1000);
-
-        cy.get('form')
-        .within(() => {
-        
-            cy.get('button').contains('Удалить').click()
-          
-        })
-
-
-
-       
-
-        cy.get('div[class*="circle_content"]').then((items) => {
-            cy.get(items[0]).within(() => {
-                cy.get('div[class*="circle_circle"]').invoke('attr', 'class').then(classList => expect(classList).contains('circle_changing'))
-            })
-            
-        })
-
-        cy.tick(1000)
-
-        cy.get('div[class*="circle_content"]').then((items) => {
-            cy.get(items[0]).within(() => {
-                cy.get('div[class*="circle_circle"]').should('have.text', '')
-            })
-
-            cy.get(items[1]).contains('head').should('have.text', 'head');
-            cy.get(items[2]).contains('tail').should('have.text', 'tail');
-            
-            
-        })
-
-
-
-        it('clear queue', () => {
-            cy.clock()
-            cy.get('form')
-            .within(() => {
-                cy.get('input').type('1')
-                cy.get('button[type="submit"]').click()
-                cy.tick(1000)
-                cy.get('input').type('2')
-                cy.get('button[type="submit"]').click()
-                cy.tick(1000)
-                cy.get('input').type('3')
-                cy.get('button[type="submit"]').click()
-            })
-
-            cy.tick(1000)
-
-            cy.get('form')
-            .within(() => {
-                
-                cy.get('button').contains('Очистить').click()
-               
-            })
-
-
-            cy.get('div[class*="circle_circle"]').then((items) => {
-
-                for(let i = 0; i < items.length; i++) {
-                    cy.get(items[i]).should('have.text', '');
-                }
-            })
-
-    
-    
-        })
-
-
-
-    })
-
-
-})
+      cy.get('div[class*="circle_circle"]').then((items) => {
+        for (let i = 0; i < items.length; i++) {
+          cy.get(items[i]).should("have.text", "");
+        }
+      });
+    });
+  });
+});
--- cypress/e2e/string.spec.cy.js
+++ cypress/e2e/string.spec.cy.js
@@ -1,125 +1,93 @@
+describe("string page logic correct", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/recursion");
+  });
 
+  it("Submit button disabled when input is empty", () => {
+    cy.get("form").within(() => {
+      cy.get("input").should("have.value", "");
+      cy.get('button[type="submit"]').should("be.disabled");
+    });
+  });
 
-describe('string page logic correct', () => {
+  it("String reverse correct", async () => {
+    cy.clock();
+    cy.get("form").within(() => {
+      cy.get("input").type("012");
+      cy.get('button[type="submit"]').click();
+    });
 
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      for (let i = 0; i < items.length; i++) {
+        cy.get(items[i])
+          .children()
+          .should("have.text", i);
+        cy.get(items[i])
+          .invoke("attr", "class")
+          .then((classList) => expect(classList).contains("circle_default"));
+      }
+    });
 
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/recursion')
-    })
+    cy.tick(1000);
 
-    it('Submit button disabled when input is empty', () => {
-      
-        cy.get('form')
-                .within(() => {
-                    cy.get('input').should('have.value', '')
-                    cy.get('button[type="submit"]').should('be.disabled')
-                })
-    })
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      cy.get(items[0])
+        .children()
+        .should("have.text", "0");
+      cy.get(items[0])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains("circle_changing"));
+    });
 
-    it('String reverse correct', async () => {
-        cy.clock()
-        cy.get('form').within(() => {
-            cy.get('input').type('012')
-            cy.get('button[type="submit"]').click()
-        })
+    cy.tick(1000);
 
-        
-        cy.get('div[class*="circle_circle"]').then((items) => {
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      cy.get(items[0])
+        .children()
+        .should("have.text", "2");
+      cy.get(items[0])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains("circle_modified"));
+    });
 
-            
-            for ( let i = 0; i < items.length; i++) {
-                cy.get(items[i]).children().should('have.text', i)
-                cy.get(items[i])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_default'))
-            }
-            
-           
+    cy.tick(1000);
 
-        })
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      cy.get(items[1])
+        .children()
+        .should("have.text", "1");
+      cy.get(items[1])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains("circle_changing"));
+    });
 
-        
-        cy.tick(1000)
+    cy.tick(1000);
 
-        cy.get('div[class*="circle_circle"]').then((items) => {
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      cy.get(items[1])
+        .children()
+        .should("have.text", "1");
+      cy.get(items[1])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains("circle_modified"));
+    });
 
+    cy.get('div[class*="circle_circle"]').then((items) => {
+      const result = ["2", "1", "0"];
 
-            cy.get(items[0]).children().should('have.text', '0')
-            cy.get(items[0])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_changing'))
-           
+      for (let i = 0; i < items.length; i++) {
+        cy.get(items[i])
+          .children()
+          .should("have.text", result[i]);
+        cy.get(items[i])
+          .invoke("attr", "class")
+          .then((classList) => expect(classList).contains("circle_modified"));
+      }
+    });
 
-        })
-
-        cy.tick(1000)
-
-        cy.get('div[class*="circle_circle"]').then((items) => {
-
-
-            cy.get(items[0]).children().should('have.text', '2')
-            cy.get(items[0])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_modified'))
-           
-
-        })
-
-      
-
-      
-        cy.tick(1000)
-
-        cy.get('div[class*="circle_circle"]').then((items) => {
-
-
-            cy.get(items[1]).children().should('have.text', '1')
-            cy.get(items[1])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_changing'))
-           
-
-        })
-      
-        cy.tick(1000)
-
-        cy.get('div[class*="circle_circle"]').then((items) => {
-
-
-            cy.get(items[1]).children().should('have.text', '1')
-            cy.get(items[1])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_modified'))
-           
-
-        })
-
-       
-        cy.get('div[class*="circle_circle"]').then((items) => {
-
-            const result = ['2', '1', '0']
-
-            for ( let i = 0; i < items.length; i++) {
-                cy.get(items[i]).children().should('have.text', result[i])
-                cy.get(items[i])
-                .invoke('attr', 'class')
-				.then(classList => expect(classList).contains('circle_modified'))
-            }
-            
-
-        })
-
-        cy.get('form')
-        .within(() => {
-            cy.get('input').should('have.value', '')
-            cy.get('button[type="submit"]').should('be.disabled')
-        })
-
-
-    })
-
-
-
-
-
-})
+    cy.get("form").within(() => {
+      cy.get("input").should("have.value", "");
+      cy.get('button[type="submit"]').should("be.disabled");
+    });
+  });
+});
